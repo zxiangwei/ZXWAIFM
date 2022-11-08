83ed485d55795065614e3d9d02db12b6802df927
zhj12138 <1550223143@qq.com>
2022-11-08 23:13:22 +0800

    build: try to build aifm library by CMake(try12)

--- aifm/run_test.sh
+++ aifm/run_test.sh
@@ -1,12 +1,16 @@
-function run_single_test {
-    echo "Running test $1..."
-    rerun_local_iokerneld
-    if [[ $1 == *"tcp"* ]]; then
-    	rerun_mem_server
-    fi
-    if run_program ./bin/$1 2>/dev/null | grep -q "Passed"; then
-        say_passed
-    else
-        say_failed
-    fi
-}
+#!/bin/bash
+
+source shared.sh
+
+echo "Running test $1..."
+rerun_local_iokerneld
+if [[ $1 == *"tcp"* ]]; then
+  rerun_mem_server
+fi
+if run_program ./bin/$1 2>/dev/null | grep -q "Passed"; then
+    say_passed
+    return 0
+else
+    say_failed
+    return 1
+fi
