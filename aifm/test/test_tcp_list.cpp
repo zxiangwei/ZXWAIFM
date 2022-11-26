68b8f240db68512cb845d0398af95408963eaefa
zhj12138 <1550223143@qq.com>
2022-11-26 14:11:44 +0800

    fix: 尝试修复tcp_list的代码

--- aifm/test/test_tcp_list.cpp
+++ aifm/test/test_tcp_list.cpp
@@ -12,54 +12,60 @@ extern "C" {
 
 using namespace far_memory;
 
-constexpr uint64_t kCacheSize = (1ULL << 10);
+constexpr uint64_t kCacheSize = (128ULL << 10);
 constexpr uint64_t kFarMemSize = (4ULL << 30);
 constexpr uint32_t kNumGCThreads = 12;
 constexpr int kListSize = 10000000;
+constexpr static uint32_t kNumConnections = 300;
 
-namespace far_memory {
-
-class FarMemTest {
- public:
-  void do_work(FarMemManager *manager) {
-    std::cout << "Running " << __FILE__ "..." << std::endl;
-    DerefScope scope;
-    List<int> list = FarMemManagerFactory::get()->allocate_list<int>(
-        scope, /* enable_merge = */ true);
-    for (int i = 0; i < kListSize; ++i) {
-      list.push_back(scope, i);
-    }
-    auto start = std::chrono::high_resolution_clock::now();
-    for (auto iter = list.begin(scope); iter != list.end(scope);
-         iter.inc(scope)) {
-      int val = iter.deref(scope);
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end-start;
-    std::ofstream ofs("time.txt");
-    ofs << "Iter cost " << diff.count() << " s\n";
-    std::cout << "Passed" << std::endl;
+void do_work(FarMemManager *manager) {
+  std::cout << "Running " << __FILE__ "..." << std::endl;
+  DerefScope scope;
+  List<int> list = FarMemManagerFactory::get()->allocate_list<int>(
+      scope, /* enable_merge = */ true);
+  for (int i = 0; i < kListSize; ++i) {
+    list.push_back(scope, i);
   }
-};
-} // namespace far_memory
-
-void _main(void *arg) {
-  std::unique_ptr<FarMemManager> manager =
-      std::unique_ptr<FarMemManager>(FarMemManagerFactory::build(
-          kCacheSize, kNumGCThreads, new FakeDevice(kFarMemSize)));
-  FarMemTest test;
-  test.do_work(manager.get());
+  auto start = std::chrono::high_resolution_clock::now();
+  for (auto iter = list.begin(scope); iter != list.end(scope);
+       iter.inc(scope)) {
+    int val = iter.deref(scope);
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  std::ofstream ofs("time.txt");
+  ofs << diff.count() << " s\n";
+  std::cout << "Passed" << std::endl;
 }
 
-int main(int argc, char *argv[]) {
+int argc;
+void _main(void *arg) {
+  char **argv = static_cast<char **>(arg);
+  std::string ip_addr_port(argv[1]);
+  auto raddr = helpers::str_to_netaddr(ip_addr_port);
+  std::unique_ptr<FarMemManager> manager =
+      std::unique_ptr<FarMemManager>(FarMemManagerFactory::build(
+          kCacheSize, kNumGCThreads,
+          new TCPDevice(raddr, kNumConnections, kFarMemSize)));
+  do_work(manager.get());
+}
+
+int main(int _argc, char *argv[]) {
   int ret;
 
-  if (argc < 2) {
-    std::cerr << "usage: [cfg_file]" << std::endl;
+  if (_argc < 3) {
+    std::cerr << "usage: [cfg_file] [ip_addr:port]" << std::endl;
     return -EINVAL;
   }
 
-  ret = runtime_init(argv[1], _main, NULL);
+  char conf_path[strlen(argv[1]) + 1];
+  strcpy(conf_path, argv[1]);
+  for (int i = 2; i < _argc; i++) {
+    argv[i - 1] = argv[i];
+  }
+  argc = _argc - 1;
+
+  ret = runtime_init(conf_path, _main, argv);
   if (ret) {
     std::cerr << "failed to start runtime" << std::endl;
     return ret;
