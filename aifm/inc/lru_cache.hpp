361d33902850683c123121940f8d1a83ac1f80e9
zhj12138 <1550223143@qq.com>
2022-12-31 14:03:38 +0800

    feat: Disable lru_cache

--- aifm/inc/lru_cache.hpp
+++ aifm/inc/lru_cache.hpp
@@ -5,7 +5,7 @@
 
 template<typename Key, typename Value>
 class LRUCache {
-  static constexpr bool kDisabled = false;
+  static constexpr bool kDisabled = true;
  public:
 
   explicit LRUCache(int capacity) : capacity_(capacity) {
