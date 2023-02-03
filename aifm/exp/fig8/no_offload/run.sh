d02fa2927680681e7d9baaf5966a0aa9b9a89a8d
zhj12138 <1550223143@qq.com>
2023-02-03 15:27:16 +0800

    build: change script

--- aifm/exp/fig8/no_offload/run.sh
+++ aifm/exp/fig8/no_offload/run.sh
@@ -5,7 +5,8 @@ log_folder=`pwd`
 cd ../../../
 source shared.sh
 
-cache_sizes=(1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31)
+cache_sizes=(1)
+ #3 5 7 9 11 13 15 17 19 21 23 25 27 29 31)
 
 sudo pkill -9 main
 
