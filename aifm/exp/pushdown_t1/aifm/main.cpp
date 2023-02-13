c7aa8df4b191d60f30b5464964713433e4522d7d
zhj12138 <1550223143@qq.com>
2023-02-13 12:45:42 +0800

    feat: bench flush

--- aifm/exp/pushdown_t1/aifm/main.cpp
+++ aifm/exp/pushdown_t1/aifm/main.cpp
@@ -139,8 +139,9 @@ void fm_compress_files_bench(const string &in_file_path,
   auto start = chrono::steady_clock::now();
   for (uint32_t i = 0; i < kNumUncompressedFiles; i++) {
     std::cout << "Compressing file " << i << std::endl;
-    snappy::Compress<kUncompressedFileNumBlocks, kUseTpAPI>(
-        fm_array_ptrs[i].get(), kUncompressedFileSize, &out_str);
+    fm_array_ptrs[i]->flush();
+//    snappy::Compress<kUncompressedFileNumBlocks, kUseTpAPI>(
+//        fm_array_ptrs[i].get(), kUncompressedFileSize, &out_str);
 //    do_something<kUncompressedFileNumBlocks, kUseTpAPI>(
 //        fm_array_ptrs[i].get(), kUncompressedFileSize, &out_str);
 //    bench_farmem_load<kUncompressedFileNumBlocks, kUseTpAPI>(
