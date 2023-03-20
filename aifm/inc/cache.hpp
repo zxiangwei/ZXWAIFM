4a64110e6f7cbe55e327e00e089c8fc179054a48
zhj12138 <1550223143@qq.com>
2023-03-20 21:08:17 +0800

    feat: enable

--- aifm/inc/cache.hpp
+++ aifm/inc/cache.hpp
@@ -8,7 +8,7 @@
 
 template<typename Key, typename Value>
 class Cache {
-  static constexpr bool kDisabled = true;
+  static constexpr bool kDisabled = false;
   using Impl = detail::CacheImpl<Key, Value>;
  public:
   static Cache LRU(size_t capacity) {
