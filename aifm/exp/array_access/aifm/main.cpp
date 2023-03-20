78938d62627e73a98c40ba7b81c291740b139cfe
zhj12138 <1550223143@qq.com>
2023-03-20 20:38:56 +0800

    feat: disable cache

--- aifm/exp/array_access/aifm/main.cpp
+++ aifm/exp/array_access/aifm/main.cpp
@@ -29,7 +29,7 @@ constexpr uint64_t kUncompressedFileNumBlocks =
     ((kUncompressedFileSize - 1) / snappy::FileBlock::kSize) + 1;
 constexpr uint32_t kNumUncompressedFiles = 16;
 constexpr bool kUseTpAPI = false;
-constexpr uint64_t kReadFileBlockNum = 1024;
+constexpr uint64_t kReadFileBlockNum = 2048;
 
 using namespace std;
 
