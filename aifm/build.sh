837fabe36cc78da5f514d818ffa37c4c4c799e96
zxiangwei <44313751+zxiangwei@users.noreply.github.com>
2023-04-03 08:46:14 +0000

    change build.sh

--- aifm/build.sh
+++ aifm/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Far-Mem Snappy.
+cd snappy
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. || { echo 'Failed to build Snappy.'; exit 1; }
+make -j
+cd ..
+
 # AIFM Core.
 make clean
 make -j$(nproc) || { echo 'Failed to build AIFM Core.'; exit 1; }
